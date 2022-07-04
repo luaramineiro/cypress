5e168f352e2c561f201fabda621277afaa39e1f0
Luara Mineiro <luara.mineiro@thoughtworks.com>
2022-07-04 19:21:55 -0300

    Cypress 10 + cucumber

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+const cucumber = require('cypress-cucumber-preprocessor').default;
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      on('file:preprocessor', cucumber());
+    },
+    specPattern: "**/*.{feature,features}",
+  },
+});
---
+++ cypress/e2e/google.feature
@@ -0,0 +1,7 @@
+Feature: Google Main Page
+
+  I want to open a search engine
+  
+  Scenario: Opening a search engine page
+    Given I open Google page
+    Then I see "Google" in the title
