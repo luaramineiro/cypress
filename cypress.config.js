0f05a718cf77d9b2054e47b18651b476858a2309
Luara Mineiro <luara.mineiro@thoughtworks.com>
2022-08-12 21:36:00 -0300

    Parallelization test

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,12 @@
 const { defineConfig } = require("cypress");
-const cucumber = require('cypress-cucumber-preprocessor').default;
+const cucumber = require("cypress-cucumber-preprocessor").default;
 
 module.exports = defineConfig({
+  projectId: "p3a2wa",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      on('file:preprocessor', cucumber());
+      on("file:preprocessor", cucumber());
     },
     specPattern: "**/*.{feature,features}",
   },
